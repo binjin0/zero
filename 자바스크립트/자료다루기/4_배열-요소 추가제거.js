df5c5037bf9e3ec0dda2edc3b3813e09c60f47ae
Hyo Bin <a032875@gmail.com>
2023-11-20 23:09:55 +0900

    배열

--- 자바스크립트/자료다루기/4_배열-요소 추가제거.js
+++ 자바스크립트/자료다루기/4_배열-요소 추가제거.js
@@ -4,10 +4,13 @@ push => 배열의 끝에 요소 추가
 shift => 배열의 앞에 요소 제거
 pop => 배열의 끝에 요소 제거
 splice => 배열의 인덱스를 기반으로 요소 추가 및 삭제
+
+
+---->이러한 메소드들은 원본배열을 훼손함.즉 새로운 배열 만들지 못함. 따라서 사용할때 주의해서 사용하자.
  */
 
 const arr = ["one", "two", "three"];
-const copyArr = arr;
+const copyArr = arr; //원본배열 복사
 
 arr.push(1);
 arr.push(2);
