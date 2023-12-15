84cf8d8f2a3bb9cb0ad8213c86215b6fdac41a04
Hyo Bin <a032875@gmail.com>
2023-12-15 23:00:55 +0900

    과제1최종

--- JS과제1/template/app.js
+++ JS과제1/template/app.js
@@ -1,1 +1,13 @@
 // do something!
+window.addEventListener("load", function () {
+  const toggleButton = document.querySelector(".toggle");
+  const nav = document.querySelector("nav");
+
+  // 토글 버튼 클릭 시 내비게이션 바 활성화/비활성화 토글
+  toggleButton.addEventListener("click", function () {
+    nav.classList.toggle("active"); // active 클래스를 추가/제거하여 내비게이션 표시/숨김
+  });
+
+  // 페이지 로드 후 초기화: 초기 렌더링 트랜지션 효과를 위한 클래스 제거
+  document.body.classList.remove("preload");
+});
