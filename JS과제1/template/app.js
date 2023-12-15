29f1f7a557eac7436b8741671f7cb7f8fe454922
Hyo Bin <a032875@gmail.com>
2023-12-15 23:10:38 +0900

    과제1-최최종

--- JS과제1/template/app.js
+++ JS과제1/template/app.js
@@ -1,12 +1,26 @@
-// do something!
+function saveNavigationState(state) {
+  localStorage.setItem("navigationState", state);
+}
+function getNavigationState() {
+  return localStorage.getItem("navigationState");
+}
 window.addEventListener("load", function () {
   const toggleButton = document.querySelector(".toggle");
   const nav = document.querySelector("nav");
 
-  // 토글 버튼 클릭 시 내비게이션 바 활성화/비활성화 토글
   toggleButton.addEventListener("click", function () {
-    nav.classList.toggle("active"); // active 클래스를 추가/제거하여 내비게이션 표시/숨김
+    nav.classList.toggle("active");
+
+    // 내비게이션 상태를 localStorage에 저장
+    const isActive = nav.classList.contains("active");
+    saveNavigationState(isActive);
   });
+
+  // 페이지 로드 시, localStorage에서 이전 내비게이션 상태를 가져와 적용
+  const previousState = getNavigationState();
+  if (previousState === "true") {
+    nav.classList.add("active");
+  }
 
   // 페이지 로드 후 초기화: 초기 렌더링 트랜지션 효과를 위한 클래스 제거
   document.body.classList.remove("preload");
