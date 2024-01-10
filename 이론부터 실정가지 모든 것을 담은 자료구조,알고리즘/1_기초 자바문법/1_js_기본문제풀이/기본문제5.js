a394966c71193694ee8a3ec0ba562ed09651c517
Hyo Bin <a032875@gmail.com>
2024-01-10 20:23:59 +0900

    js_기본문제

---
+++ 이론부터 실정가지 모든 것을 담은 자료구조,알고리즘/1_기초 자바문법/1_js_기본문제풀이/기본문제5.js
@@ -0,0 +1,27 @@
+/**
+* 4. 헨드폰 판매\
+업무 성과를 보상해주기 위해, 가장 핸드폰은 많이 판매한 종업원에게 인센티브를 부여하고자 한다.
+핸드폰 판매 수량을 입력 받아서, 가장 많이 판매한 종업원 번호를 반환해주는 함수를 작성하시오.
+입력은 배열 형태로 종업원 별 판매 수량 값이 주어지며, 가장 많이 판매한 번호를 반환한다.  
+***/
+
+/* user code */
+function answer(employee) {
+  let maxNumber = Math.max(...employee);
+
+  for (let i = 0; i < employee.length; i++) {
+    if (maxNumber === employee[i]) return i + 1;
+  }
+}
+/* main code */
+let input = [
+  // TC: 1
+  [3, 7, 9, 6, 1],
+  // TC: 2
+  [2, 7, 1, 4, 3, 0, 5],
+  // TC: 3
+  [7, 5, 0, 1, 2, 12, 6],
+];
+for (let i = 0; i < input.length; i++) {
+  console.log(`#${i + 1} ${answer(input[i])}`);
+}
